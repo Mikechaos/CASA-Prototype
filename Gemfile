4023c092a8a929876d8decb68530cb5721d67042
Mikechaos <mikechaos13@gmail.com>
2013-03-27 15:17:04 -0400

    Added pg to GemFile

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 gem 'sinatra', '1.4.0'
 gem "haml", ">= 3.1.4"
 gem "sequel", "3.20.0"
+gem "pg", "0.15.0"
--- models/init.rb
+++ models/init.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 require 'sequel'
-DB = Sequel.sqlite 'db/data.db'
+# DB = Sequel.sqlite 'db/data.db'
 DB = Sequel.connect(:adapter => 'postgres',
                     :host => 'ec2-107-22-165-15.compute-1.amazonaws.com',
                     :database => 'd563rs3agl1jk7',
