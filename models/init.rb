f911778b5de389340085e98f90562f69b59632f9
Mikechaos <mikechaos13@gmail.com>
2013-03-27 21:30:49 -0400

    Added proper Sequel.connect to init.rb

--- models/init.rb
+++ models/init.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 require 'sequel'
-# DB = Sequel.sqlite 'db/data.db'
-DB = Sequel.connect(:adapter => 'postgres',
-                    :host => 'ec2-107-22-165-15.compute-1.amazonaws.com',
-                    :database => 'd563rs3agl1jk7',
-                    :user => 'itvnbulwdbqpbc',
-                    :port => '5432',
-                    :password => 'm9kpPPw2GrhJ-FQVsdBcpfiZca')
+#DB = Sequel.sqlite 'db/data.db'
+# DB = Sequel.connect(:adapter => 'postgres',
+#                     :host => 'ec2-107-22-165-15.compute-1.amazonaws.com',
+#                     :database => 'd563rs3agl1jk7',
+#                     :user => 'itvnbulwdbqpbc',
+#                     :port => '5432',
+#                     :password => 'm9kpPPw2GrhJ-FQVsdBcpfiZca')
+DB = Sequel.connect('postgres://x:x@ec2-107-22-165-15.compute-1.amazonaws.com:5432/d563rs3agl1jk7')
 # DB << "SET CLIENT_ENCODING TO 'UTF8';"
 
 require_relative 'employee'
